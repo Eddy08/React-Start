c68b9c624e2952ff1d981b693c6f8f8ede62b8f1
Harsh <40041498+Eddy08@users.noreply.github.com>
2020-07-02 18:42:52 +0530

    Using Emoji 📧 in React Use of <span role="img" aria-label="emoji-name" >⚓</span>

--- src/index.js
+++ src/index.js
@@ -1,63 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+// import App from "./App";
 
 const rootElement = document.getElementById("root");
-let count=0;
+// let count = 0;
 // use of fontFamily:'in quotes' --> font-family:center;
 
-const styles={
-  fontFamily:"Times New Roman",
-  textAlign:'center'
+const styles = {
+  fontFamily: "Times New Roman",
+  textAlign: "center"
 };
 
-const App2 = (props) => (
-  <div style={styles}>
-  <h2>{props.count}</h2>
-  </div>
-);
+// const App2 = (props) => (
+//   <div style={styles}>
+//   <h2>{props.count}</h2>
+//   </div>
+// );
 
-class App3 extends React.Component{
-  render(){
-    return (
-       <div style={styles}>
-  <h2>{this.props.count}</h2>
-  </div>
-    )
-  }
-}
+// class App3 extends React.Component{
+//   render(){
+//     return (
+//        <div style={styles}>
+//   <h2>{this.props.count}</h2>
+//   </div>
+//     )
+//   }
+// }
 
-class App4 extends React.Component{
-  constructor(props){
-    super(props)
-    this.state= {
+class App4 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       //state is an object count =Key :0: value
-      count:0,
-    }
+      count: 0
+    };
   }
-  increaseCount(){
-    this.setState(prevState=>({count:prevState.count+1}))
-    this.setState(prevState=>({count:prevState.count+1}))
+  increaseCount() {
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+    this.setState(prevState => ({ count: prevState.count + 1 }));
     // this.setState({count:this.state.count+1})
     // this.setState({count:this.state.count+1})
-    console.log(this.state.count)
+    console.log(this.state.count);
+  }
 
-  }
-  
-  render(){
+  render() {
     return (
-       <div style={styles}>
-       <h1>State Props🥳 </h1>
- {/* Cannot bind increaseCount() */}
- {/* one way */}
-  {/* <button onClick={this.increaseCount.bind(this)}>Increase</button> */}
-  {/* 2nd way */}
-  <button onClick={() =>this.increaseCount()}>Increase</button>
-  <h2>{this.state.count}</h2>
-  </div>
-    )
-
+      <div style={styles}>
+        <h1>
+          State Props{" "}
+          <span role="img" aria-label="car">
+            🚗
+          </span>{" "}
+        </h1>
+        {/* Cannot bind increaseCount() */}
+        {/* one way */}
+        {/* <button onClick={this.increaseCount.bind(this)}>Increase</button> */}
+        {/* 2nd way */}
+        <button onClick={() => this.increaseCount()}>Increase</button>
+        <h2>{this.state.count}</h2>
+      </div>
+    );
   }
 }
 
@@ -77,8 +80,7 @@ class App4 extends React.Component{
 
 ReactDOM.render(
   <React.StrictMode>
-  <App4 />
+    <App4 />
   </React.StrictMode>,
   rootElement
-)
-
+);
